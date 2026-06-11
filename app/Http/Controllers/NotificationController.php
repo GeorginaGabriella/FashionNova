f53ecbb8066f58c052f32fe4dcbfdf1ab7a1f43d
Thender9595 <ultramanx.desu@gmail.com>
2026-06-11 08:18:15 +0700

    Fix notification controller syntax error

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,21 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Notification;
-use Illuminate\Support\Facades\Auth;
-
 class NotificationController extends Controller
 {
     public function index()
-{
-    return view('notifications.index', [
-        'notifications' => collect()
-    ]);
-}
-
-        return view(
-            'notifications.index',
-            compact('notifications')
-        );
+    {
+        return view('notifications.index', [
+            'notifications' => collect()
+        ]);
     }
 }
